17d4df7bb00eb704a607656a0ea154b85eb80b34
sangryul <ksl970330@naver.com>
2021-10-10 12:32:30 +0900

    add materio-free-vuetify template

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,38 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <component :is="resolveLayout">
+    <router-view></router-view>
+    <upgrade-to-pro></upgrade-to-pro>
+  </component>
 </template>
 
 <script>
-export default {
-  name: "App",
+import { computed } from '@vue/composition-api'
+import { useRouter } from '@/utils'
+import LayoutBlank from '@/layouts/Blank.vue'
+import LayoutContent from '@/layouts/Content.vue'
+import UpgradeToPro from './components/UpgradeToPro.vue'
 
-  data: () => ({
-    //
-  }),
-};
+export default {
+  components: {
+    LayoutBlank,
+    LayoutContent,
+    UpgradeToPro,
+  },
+  setup() {
+    const { route } = useRouter()
+
+    const resolveLayout = computed(() => {
+      // Handles initial route
+      if (route.value.name === null) return null
+
+      if (route.value.meta.layout === 'blank') return 'layout-blank'
+
+      return 'layout-content'
+    })
+
+    return {
+      resolveLayout,
+    }
+  },
+}
 </script>
